3bcc2ae929f989af951dff26efb7de149419d219
Camillus Anko <ankocamillus@gmail.com>
2025-07-20 10:11:07 +0000

    created a non-interactive web app

---
+++ web.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+from functions import get_todos
+
+todos = get_todos()
+print(todos)
+st.title("My Todo App")
+st.subheader("This is my todo app")
+
+st.write("This app is to increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+
+
+st.text_input(label="Enter a todo", placeholder="Add new todo...")
+
